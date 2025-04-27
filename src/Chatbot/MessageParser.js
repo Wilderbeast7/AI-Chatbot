1b26d4c845ae9e480a8d96fb0b6345ae052f700d
Rishabh Ahuja <150649600+Wilderbeast7@users.noreply.github.com>
2025-04-27 17:53:02 +0530

    Update MessageParser.js

--- src/Chatbot/MessageParser.js
+++ src/Chatbot/MessageParser.js
@@ -1,36 +1,21 @@
 import React from 'react';
 
 const MessageParser = ({ children, actions }) => {
-    // console.log(children.props.state)
-    const { checker } = children.props.state;
-    const parse = (message) => {
-        if (checker === "age") {
-            actions.afterNameMessage();
-            children.props.state.userData.name = message;
-        }
 
-        if (checker === "preference" && Number(message)) {
-            actions.afterAgeMessage();
-            children.props.state.userData.age = message;
-            if (message <= 10) {
-                children.props.state.userData.category = "kid";
-            } else if (message > 10 && message <= 20) {
-                children.props.state.userData.category = "teenagers";
-            } else {
-                children.props.state.userData.category = "adults";
-            }
-        }
-    }
-    return (
-        <div>
-            {React.Children.map(children, (child) => {
-                return React.cloneElement(child, {
-                    parse: parse,
-                    actions,
-                });
-            })}
-        </div>
-    );
+  const parse = (message) => {
+    if (message.trim() === "") return;
+
+    actions.answerUserQuestion(message);
+  };
+
+  return (
+    <>
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, { parse, actions })
+      )}
+    </>
+  );
 };
 
 export default MessageParser;
+
